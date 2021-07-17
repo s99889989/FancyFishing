6278ff759aa08a107b08159d2d7d9935b585c163
Daxton <s02048898067@gmail.com>
2021-07-18 04:11:31 +0800

    1

---
+++ src/main/java/com/daxton/fancyfishing/FancyFishing.java
@@ -0,0 +1,18 @@
+package com.daxton.fancyfishing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FancyFishing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        this.getLogger().info("釣魚插件2");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
